a05d68de920f6c3499a87754072ea86abdf56635
Trev <58667461+TheTrueTree@users.noreply.github.com>
2025-12-01 22:04:52 +0000

    Part 2 Attempt
    Not finished

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,15 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn returnPosition(l: i32, r: i32, m: char) -> i32 {
+fn return_position(l: i32, r: i32, m: char) -> i32 {
     let x:i32 = if m == 'R' {l + r} else {if l - r < 0 {(l - r) % 100 + 100} else {l - r}};
     return x % 100;
+}
+
+fn return_overload(l: i32, r: i32, m: char) -> u32{
+    let x:i32 = if m == 'R' {l + r} else {l - r};
+    let overload:i32 = x.abs() / 100;
+    return overload as u32;
 }
 
 fn main() {
@@ -14,19 +20,24 @@ fn main() {
     let mut password: u32 = 0;
 
     for line in reader.lines(){
+        let mut overloads: u32 = 0;
         let command = line.expect("Bad practice unuseful message");
         let operator = command.chars().next().unwrap();
         
         let number = command.replace(operator, "");
         let number = number.parse::<i32>().unwrap();
 
-        println!("Start: {}, Command: {}", starting_position, command);
-
-        starting_position = returnPosition(starting_position, number as i32, operator);
+        starting_position = return_position(starting_position, number as i32, operator);
+        overloads = return_overload(starting_position, number as i32, operator);
     
         if starting_position == 0 {
             password = password + 1;
-        }
+        } 
+        
+        password = password + overloads;
+
+        println!("Start: {}, Command: {}, Overloads: {}, Password: {}", starting_position, command, overloads, password);
+        
     }
 
     println!("Password: {}", password)
