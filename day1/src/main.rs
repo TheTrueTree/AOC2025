cb8bb91b58fd9403385b9206a80e3eba677f814f
Trev <58667461+TheTrueTree@users.noreply.github.com>
2025-12-01 21:52:36 +0000

    Fixed something I didn't realise I broke (Rogue modulo)

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,13 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn return_position(l: i32, r: i32, m: char) -> i32 {
-    let x:i32 = if m == 'R' {l + r} else {if l - r % 100 < 0 {l - r % 100 + 100} else {l - r}};
+fn returnPosition(l: i32, r: i32, m: char) -> i32 {
+    let x:i32 = if m == 'R' {l + r} else {if l - r < 0 {(l - r) % 100 + 100} else {l - r}};
     return x % 100;
 }
 
 fn main() {
-    let reader = File::open("F:/Projects/AoC2025/day1/src/input.txt").expect("Should work.");
+    let reader = File::open("F:/Projects/AoC2025/AOC2025/day1/src/input.txt").expect("Should work.");
     let reader = BufReader::new(reader);
 
     let mut starting_position: i32 = 50;
@@ -20,7 +20,10 @@ fn main() {
         let number = command.replace(operator, "");
         let number = number.parse::<i32>().unwrap();
 
-        starting_position = return_position(starting_position, number as i32, operator);
+        println!("Start: {}, Command: {}", starting_position, command);
+
+        starting_position = returnPosition(starting_position, number as i32, operator);
+    
         if starting_position == 0 {
             password = password + 1;
         }
